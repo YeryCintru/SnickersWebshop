52f11bdc78d31ed8bd019e70400b513431bd9b42
YeryCintru <137061168+YeryCintru@users.noreply.github.com>
2024-11-08 16:54:05 +0100

    Add files via upload

---
+++ PHP/aboutus.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+    <link rel="stylesheet" href="..\CSS\styles.css">
+    <link rel="stylesheet" href="..\CSS\bootstrap\css\bootstrap.min.css">
+
+
+</head>
+<body>
+
+<?php include 'header.php';?>
+
+
+<main>
+
+<div class="d-flex justify-content-center align-items-center full-height">
+            <div class="text-center">
+                <h1><strong>About us</strong></h1>
+            </div>
+</div>
+
+
+<div class="grid text-center" style="--bs-rows: 3; --bs-columns: 3;width:50%">
+
+  <div class="g-start-2" style="grid-row: 2">
+
+    <div id="carouselExampleFade" class="carousel slide carousel-fade">
+
+    <div class="carousel-inner"  style="border-radius: 15px">
+        <div class="carousel-item active">
+        <img src="..\Images\basketball.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="..\Images\streetwear.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="..\Images\imageHomePage.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+
+
+  </div>
+
+  <div class="g-start-3" style="grid-row: 3">
+
+
+  <p>At UrbanKicks, we bring the streets to your feet. Our passion for urban style drives us to curate the latest and most unique footwear collections, blending comfort, quality, and edge. From classic streetwear staples to fresh, innovative designs, our shoes are crafted to keep you moving in style. Whether you’re stepping into the everyday or standing out at night, UrbanKicks is where style meets the street.</p>
+  </div>
+
+
+</div>
+
+
+</main>
+
+
+<?php include 'footer.php'; ?>
+
+<script src="..\JS\bootstrap\js\bootstrap.bundle.min.js"></script>
+
+    
+</body>
+</html>
