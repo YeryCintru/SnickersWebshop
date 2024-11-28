9ff4c2dee0199257b8aeac5c47ecb6063770878e
javichu184 <104259747+javichu184@users.noreply.github.com>
2024-11-28 14:09:32 +0100

    a

--- PHP/header.php
+++ PHP/header.php
@@ -7,7 +7,8 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> </title>
+    
+    <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="..\CSS\styles.css">
     <link rel="stylesheet" href="..\CSS\bootstrap\css\bootstrap.min.css">
 </head>
--- PHP/index.php
+++ PHP/index.php
@@ -1,20 +1,8 @@
 <?php
 session_start();
+$title = "Snickers Login";
+include 'header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snickers Login</title>
-    <link rel="stylesheet" href="..\CSS\styles.css">
-    <link rel="stylesheet" href="..\CSS\bootstrap\css\bootstrap.min.css">
-</head>
-
-<body>
-    <?php include 'header.php'; ?>
-
     <main>
         <div>
             <div class="row justify-content-center align-items-center">
@@ -45,43 +33,6 @@ session_start();
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
-
-
-            <?php
-            require 'database.php';
-
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $username = trim($_POST['username']);
-                $password = trim($_POST['password']);
-
-                // Verificar si la contraseña cumple con los requisitos
-                if (strlen($password) < 9) {
-                    echo "La contraseña debe tener al menos 9 caracteres.<br>";
-                } elseif (!preg_match('/[A-Z]/', $password)) {
-                    echo "La contraseña debe contener al menos una letra mayúscula.<br>";
-                } elseif (!preg_match('/[a-z]/', $password)) {
-                    echo "La contraseña debe contener al menos una letra minúscula.<br>";
-                } elseif (!preg_match('/\d/', $password)) {
-                    echo "La contraseña debe contener al menos un número.<br>";
-                } else {
-                    // Buscar el usuario en la base de datos
-                    $stmt = $pdo->prepare('SELECT * FROM users WHERE username = :username');
-                    $stmt->execute(['username' => $username]);
-                    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                    if ($user && password_verify($password, $user['password'])) {
-                        // Si las credenciales son válidas, iniciar sesión
-                        $_SESSION['user_id'] = $user['IDuser'];  // Utiliza la columna correcta para el ID
-                        $_SESSION['username'] = $user['username'];
-                        header('Location: homePage.php'); // Redirige a la página principal
-                        exit;
-                    } else {
-                        echo "Usuario o contraseña incorrectos.<br>";
-                    }
-                }
-            }
-            ?>
-
 
 
 
