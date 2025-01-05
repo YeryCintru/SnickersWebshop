282f7a09caf89097077b25c33a5a9855b4fb64e5
javier-her <reydemonio12321@gmail.com>
2025-01-05 15:51:04 +0100

    register

--- PHP/register.php
+++ PHP/register.php
@@ -7,7 +7,6 @@ include 'dependences_php/headImport.php';
 ?>
 <main>
     <div class="row justify-content-center">
-
         <div>
             <div class="row justify-content-center align-items-center">
                 <div class="col-md-7 text-center">
@@ -18,52 +17,47 @@ include 'dependences_php/headImport.php';
         </div>
 
         <div class="col-md-4">
-    <form method="POST" id="signupForm">
-        <div class="mb-3">
-            <label for="InputFirstName" class="form-label">First Name</label>
-            <input type="text" class="form-control" id="first_name" name="first_name" required>
-        </div>
+            <!-- Display the error message if it exists -->
+            <?php if (isset($_SESSION['email_error'])): ?>
+                <div class="alert alert-danger">
+                    <?php
+                    // Show the error message and then clear it from the session
+                    echo $_SESSION['email_error'];
+                    unset($_SESSION['email_error']);
+                    ?>
+                </div>
+            <?php endif; ?>
 
-        <div class="mb-3">
-            <label for="InputLastName" class="form-label">Last Name</label>
-            <input type="text" class="form-control" id="last_name" name="last_name" required>
-        </div>
-
-        <div class="mb-3">
-            <label for="email" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="email" name="email" required>
-        </div>
-
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
-<!--
+            <!-- Registration form -->
+            <form method="POST" id="signupForm">
                 <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" name="password" >
+                    <label for="InputFirstName" class="form-label">First Name</label>
+                    <input type="text" class="form-control" id="first_name" name="first_name" required>
                 </div>
 
                 <div class="mb-3">
-                    <label for="repeatPassword" class="form-label">Repeat Password</label>
-                    <input type="password" class="form-control" id="repeat_password" name="repeat_password" >
+                    <label for="InputLastName" class="form-label">Last Name</label>
+                    <input type="text" class="form-control" id="last_name" name="last_name" required>
                 </div>
--->
+
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
-
-
-
         </div>
 
         <div class="row justify-content-center">
             <div class="col-md-4 text-center">
                 <p>Do you have already an account? <a href="index.php">Sign in here</a></p>
             </div>
-
         </div>
     </div>
-
-
 </main>
+
+
 <?php include 'dependences_php/footImport.php'; ?>
 <script src="..\JS\validationRegister.js" defer></script>
 </body>
